5d2910f7da2ab421c623408ed6b4660e396261f0
Kahchara <lukas.wahlgren98@hotmail.com>
2024-11-04 10:49:38 +0100

    Update Ping.py

--- Ping.py
+++ Ping.py
@@ -1,8 +1,10 @@
+#The necessary libraries to run the code
+##------------------------------------------------------##
 import argparse
 import subprocess  
 
+#The ping_sweep function that will be responsible for sending out the pings to the targeted IP-adresses
 ##-------------------------------------------------------##
-
 def ping_sweep(start_ip, end_ip):
     start = list(map(int, start_ip.split('.')))
     end = list(map(int, end_ip.split('.')))
@@ -10,13 +12,14 @@ def ping_sweep(start_ip, end_ip):
     active_hosts = []
 
     print(f"Scanning ip:{start_ip} to {end_ip}")
-
+    #This part is responsible for looping through all of the IP-adresses and ensures that they are formatted correctly
     for i in range(start[3], end[3] + 1):
-        ip = f"{start[0].start[1].start[2]}.{i}"
+        ip = f"{start[0]}.{start[1]}.{start[2]}.{i}"
 
         try:
-            result = subprocess.run(["ping", "c", "1", ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            result = subprocess.run(["ping", "-c", "1", ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+            #If the program gets a response it will indicate that the IP is active
             if result.returncode == 0:
                 print(f"[+] Host {ip} is active")
                 active_hosts.append(ip)
@@ -28,8 +31,8 @@ def ping_sweep(start_ip, end_ip):
             print(f"A problem has occured: {e}")
     return active_hosts
 
+#The main argparse function that will allow the program to be run in the terminal and will call on the previous ping_sweep function
 ##---------------------------------------------------------##
-
 def main():
     parser = argparse.ArgumentParser(description="A ping sweeper tool")
     parser.add_argument("--start-ip", required=True, help="The starting IP-adress of the range (1.1.1.1)")
@@ -38,7 +41,8 @@ def main():
     args = parser.parse_args()
 
     active_hosts = ping_sweep(args.start_ip, args.end_ip)
-
+    
+    #This will print out the results to the terminal 
     if active_hosts:
         print("\n[+] Active IP-adresses found:")
         for host in active_hosts:
