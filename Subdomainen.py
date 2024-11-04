512398cfa60c95cd9c222a20c8b79ac4b1bed592
Kahchara <lukas.wahlgren98@hotmail.com>
2024-11-04 10:19:55 +0100

    Update Subdomainen.py
    Den rätta koden

--- Subdomainen.py
+++ Subdomainen.py
@@ -1,44 +1,51 @@
+
+#Libraries necessary to run the code
+##------------------------------------------------------------##
 import argparse
 import requests
 
-##---------------------------------------------------------##
-
-def subdomain_enum():
+#The primary function for the subdomain enumerator
+##------------------------------------------------------------##
+def subdomain_enum(domain, wordlist):
     found_subdomain = []
 
+#With open will allow us to print out the found subdomains to a separate text document
     with open(wordlist, 'r') as file:
-        word = word.strip()
-        subdomain = f'http://{word}.{domain}'
+        for word in file:
+            word = word.strip()
+            subdomain = f'http://{word}.{domain}'
 
 
-        try:
-            response = requests.get(subdomain, timeout=1)
+#This try function has a timeout of one second, and if the response status==200 means if the subdomain is up and found
+            try:
+                response = requests.get(subdomain, timeout=1)
 
-            if response.status_code == 200:
-                print(f"[+] Found subdomain: {subdomain}")
-                found_subdomain.append(subdomain)
-            else:
-                print(f"[-] Not found: {subdomain}")
-        except requests.ConnectionError:
-            print(f"[-] No response from subdomain")
-        except requests.RequestException as e:
-            print(f"An error has occured: {e}")
+                if response.status_code == 200:
+                    print(f"[+] Found subdomain: {subdomain}")
+                    found_subdomain.append(subdomain)
+                else:
+                    print(f"[-] Not found: {subdomain}")
+            except requests.ConnectionError:
+                print(f"[-] No response from subdomain")
+            except requests.RequestException as e:
+                print(f"An error has occured: {e}")
     return found_subdomain
 
+#The main argparse function which will call on the subdomain enumerator above to run the program from the terminal
 ##-------------------------------------------------------------##
-
 def main():
     parser = argparse.ArgumentParser(description="This is a subdomain enumerator :)")
     parser.add_argument("--domain", required=True, help="Domain to enumerate subdomain on")
-    parser.add_argument("--worldist", required=True, help="The path to the wordlist with the subdomains you wish to test")
+    parser.add_argument("--wordlist", required=True, help="The path to the wordlist with the subdomains you wish to test")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-found = subdomain_enum(args.domain, args.wordlist)
-if found:
-    print(f"\n[+] Discovered subdomains:")
-    for sub in found:
-        print(sub)
+#This code will print the result in the terminal as well, allowing for faster confirmation of which sites are up
+    found = subdomain_enum(args.domain, args.wordlist)
+    if found:
+        print(f"\n[+] Discovered subdomains:")
+        for sub in found:
+            print(sub)
     else:
         print("[-] No subdomain found")
 
